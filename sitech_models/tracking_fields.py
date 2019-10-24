0a701f94b696437c556a5eda69667a4ec3d2f0d0
Abdulhadi Abu-Shamleh <abushamleh92@gmail.com>
2019-10-24 13:17:37 +0300

    Update tracking_fields.py
    Add get_dirty_fields method to returns the fields name that have been modified since they are loaded or saved most recently.

--- sitech_models/tracking_fields.py
+++ sitech_models/tracking_fields.py
@@ -15,10 +15,21 @@ class TrackingFieldsMixin:
             attname, column = field.get_attname_column()
             self._old_fields[attname] = getattr(self, attname)
 
+    def get_old_fields(self):
+        return self._old_fields
+
+    # Returns the fields name that have been modified since they are loaded or saved most recently.
+    def get_dirty_fields(self):
+        dirty_fields = []
+        for field in self._old_fields:
+            if self._old_fields[field] != getattr(self, field):
+                dirty_fields.append(field)
+        return dirty_fields
+
     def get_old_field(self, field, default=None):
         if field in self._old_fields:
             return self._old_fields[field]
         return default
 
     def set_old_field(self, field, value):
-        self._old_fields[field] = value
+        self._old_fields[field] = value        
